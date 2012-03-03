e7f95365253cf0fc06ab196264c272a8e922ccc0
Tomoaki OKITSU <okitsu.t@gmail.com>
2012-03-03 08:25:43 -0500

    add main

---
+++ Zabbix/Agent/Config.php
@@ -0,0 +1,42 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+if(!defined('ZABBIX_AGENT_DEFAULT_CONFIG_FILE')) {
+    define('ZABBIX_AGENT_DEFAULT_CONFIG_FILE','/etc/zabbix/zabbix_agentd.conf',true);
+}
+
+class Zabbix_Agent_Config
+{
+    var $_config_filename = ZABBIX_AGENT_DEFAULT_CONFIG_FILE;
+    var $_config_array = array();
+     
+    function Zabbix_Agent_Config($filename=null){
+        if( !is_null($filename) ){
+           $this->_config_filename = $filename;
+        }
+        $this->_config_array = $this->loadAgentConfig($this->_config_filename);
+    }
+    
+    function getAgentConfig(){
+        return $this->_config_array;
+    }    
+
+    function setConfigFilename($filename){
+        $this->_config_filename = $filename;
+    }   
+ 
+    static function loadAgentConfig($filename=null){
+        $config_array = array();
+        $config_lines = file($filename);
+        $config_lines = preg_grep("/^\s*[A-Za-z].+/",$config_lines);
+        foreach($config_lines as $line_num => $line){
+            list($key,$value) = explode("=",$line,2);
+            $key = trim($key);
+            $value = trim($value);
+            $config_array{$key} = $value; 
+        }
+        return $config_array;
+    }
+}
+
+
---
+++ Zabbix/Sender.php
@@ -0,0 +1,95 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+require_once('Agent/Config.php');
+
+# Default configuration
+if(!defined('ZABBIX_SENDER_DEFAULT_SERVERNAME')) {
+    define('ZABBIX_SENDER_DEFAULT_SERVERNAME','localhost',true);
+}
+if(!defined('ZABBIX_SENDER_DEFAULT_SERVERPORT')) {
+    define('ZABBIX_SENDER_DEFAULT_SERVERPORT',10051,true);
+}
+if(!defined('ZABBIX_SENDER_DEFAULT_CONNECTION_TIMEOUT')) {
+    define('ZABBIX_SENDER_DEFAULT_CONNECTION_TIMEOUT',30,true);
+}
+
+# ZABBIX Sender Protocol
+if(!defined('ZABBIX_SENDER_PROTOCOL_HEADER_STRING')) {
+    define('ZABBIX_SENDER_PROTOCOL_HEADER_STRING','ZBXD',true);
+}
+if(!defined('ZABBIX_SENDER_PROTOCOL_VERSION')) {
+    define('ZABBIX_SENDER_PROTOCOL_VERSION',1,true);
+}
+
+class Zabbix_Sender {
+    
+    var $_servername = ZABBIX_SENDER_DEFAULT_SERVERNAME;
+    var $_serverport = ZABBIX_SENDER_DEFAULT_SERVERPORT;
+    var $_timeout    = ZABBIX_SENDER_DEFAULT_CONNECTION_TIMEOUT;
+    var $_data; 
+
+    function Zabbix_Sender($servername = null,$serverport = null)
+    {
+        if(! is_null($servername)){
+            $this->_servername = $servername;
+        }
+        if(! is_null($serverport)){
+            $this->_serverport = $serverport;
+        }
+        $this->_data = $this->_createDataTemplate(); 
+    }
+
+    function _createDataTemplate()
+    {
+        return array(
+            "request" => "sender data",
+            "data" => array()
+        );
+    }
+
+    function importAgentConfig(Zabbix_Agent_Config $agentConfig){
+        $config = $agentConfig->getAgentConfig();
+        $this->_servername = $config{'Server'}; 
+        $this->_serverport = $config{'ServerPort'}; 
+    }
+    
+    function setTimeout($timeout){
+        if(intval($timeout) > 0){
+            $this->_timeout = $timeout;
+        }
+        return $this->_timeout;
+    }   
+ 
+    function addData($hostname=null,$key=null,$value=null)
+    {
+        array_push($this->_data{"data"},array("host"=>$hostname,"value"=>$value,"key"=>$key));
+    }
+
+    function send()
+    {
+        $json_data   = json_encode( array_map( function($t){ return is_string($t) ? utf8_encode($t) : $t; }, $this->_data ) );
+        $json_length = strlen($json_data);
+        $data_header = pack("aaaaCCCCCCCCC",
+                                substr(ZABBIX_SENDER_PROTOCOL_HEADER_STRING,0,1),
+                                substr(ZABBIX_SENDER_PROTOCOL_HEADER_STRING,1,1),
+                                substr(ZABBIX_SENDER_PROTOCOL_HEADER_STRING,2,1),
+                                substr(ZABBIX_SENDER_PROTOCOL_HEADER_STRING,3,1),
+                                intval(ZABBIX_SENDER_PROTOCOL_VERSION),
+                                ($json_length & 0xFF),
+                                ($json_length & 0x00FF)>>8,
+                                ($json_length & 0x0000FF)>>16,
+                                ($json_length & 0x000000FF)>>24,
+                                0x00,
+                                0x00,
+                                0x00,
+                                0x00
+                            );
+        $sock = fsockopen($this->_servername,intval($this->_serverport),$errno,$errmsg,$this->_timeout);
+        fputs($sock,$data_header);
+        fputs($sock,$json_data);
+        fclose($sock);
+    }
+}
+
+
