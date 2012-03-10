ea002f67081562c91e5c9a10128b89ee934545e2
Tomoaki OKITSU <okitsu.t@gmail.com>
2012-03-11 02:04:00 +0900

    src/Net/Zabbix/Sender.php

--- tests/Net/Zabbix/SenderTest.php
+++ tests/Net/Zabbix/SenderTest.php
@@ -4,7 +4,9 @@ class Zabbix_SenderTest extends \PHPUnit_Framework_TestCase
 {
 	public function setUp()
 	{
-		$this->sender = new Net\Zabbix\Sender;
+		$this->sender = new Net\Zabbix\Sender('localhost',10051);
+		$agentConfig = new Net\Zabbix\Agent\Config();
+		$this->sender->importAgentConfig($agentConfig);
 	}
 	
 	public function test_set_getTimeout()
@@ -23,13 +25,14 @@ class Zabbix_SenderTest extends \PHPUnit_Framework_TestCase
 	function _addData(\Net\Zabbix\Sender $sender){
 		$sender->addData("hostname1","key1","value1");	
 		$sender->addData("hostname2","key2","value2");
+		$sender->addData("hostname3","key3","value3",1234567890);
 	}
 	
 	public function test_addData()
 	{
 		$this->_addData($this->sender);
 		$dataArray = $this->sender->getDataArray();
-		$this->assertCount(2,$dataArray);
+		$this->assertCount(3,$dataArray);
 	}
 
 	public function test_unsetData()
@@ -39,5 +42,27 @@ class Zabbix_SenderTest extends \PHPUnit_Framework_TestCase
 		$dataArray = $this->sender->getDataArray();
 		$this->assertCount(0,$dataArray);
 	}
-
+	
+	public function test_send_fail()
+	{
+		$this->sender->_data = array(); 
+		$result = $this->sender->send();
+		$this->assertFalse($result);
+	}
+	
+	public function test_send()
+	{
+		$this->_addData($this->sender);
+		$result = $this->sender->send();
+		$this->assertTrue($result);
+		$this->assertEquals(3,$this->sender->getLastFailed());
+		$this->assertEquals(0,$this->sender->getLastProcessed());
+		$this->assertEquals(3,$this->sender->getLastTotal());
+		$this->assertGreaterThanOrEqual(0.000000001,$this->sender->getLastSpent());
+		$this->assertArrayHasKey('info',$this->sender->getLastResponseArray());
+		$this->assertArrayHasKey('response',$this->sender->getLastResponseArray());
+		$this->assertRegExp('/Processed \d+ Failed \d+ Total \d+ Seconds spent \d+\.\d+/',
+					$this->sender->getLastResponseInfo());
+	}
+	
 }
